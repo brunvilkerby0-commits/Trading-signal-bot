e422e7ab71f3feb696ad676d17aa2baa07636a63
brunvilkerby0-commits <brunvilkerby0@gmail.com>
2026-08-16 13:30:27 -0400

    index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const WebSocket = require('ws');
 
-const app = express();
+const app = express(); 
 const PORT = process.env.PORT || 3000;
 
 // 🔐 Konfigirasyon Telegram ak Deriv
